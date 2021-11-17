3d7dfa65e93a35d44390dbcd10d367196db28e54
phil-forson <84267055+phil-forson@users.noreply.github.com>
2021-11-17 22:35:36 +0000

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,43 @@
+
+const cars = [
+    {
+      id: 1,
+      name: 'Ford 150',
+      price: '39,000',
+      like: true,
+      img: require('../assets/images/ford.jpg'),
+      about:
+        'Anytime there is a redesigned Ford F-150, it is kind of a big deal. After all, the F-150 has dominated vehicle sales in the United States for decades. So here we are with the redesigned 2021 F-150. There is no revolutionary shift in construction this year, unlike in 2015 when Ford debuted aluminum body panels on the F-150. Instead, Ford has focused on enhancing the trucks capability in other ways. Examples include a new hybrid powertrain, a robust onboard generator, and an updated interior with a new infotainment system.',
+    },
+  
+    {
+      id: 2,
+      name: 'Lamborghini Aventador',
+      price: '990,000',
+      like: false,
+      img: require('../assets/images/lamborghini.jpg'),
+      about:
+        'The Aventador has been created to anticipate the future, as demonstrated by the use of innovative technology, including a V12 engine and the extensive use of carbon fiber.',
+    },
+    {
+      id: 3,
+      name: 'S-class Maybach',
+      price: '56,000',
+      like: false,
+      img: require('../assets/images/s-class.jpg'),
+      about:
+        'With a starting price of around $185,000, the Maybach S580 carries a nearly $25,000 premium over the similarly-posh Maybach GLS600 SUV. The sedan is powered by a 496-hp twin-turbo V-8 with a 48-volt hybrid system.',
+    },
+  
+    {
+      id: 4,
+      name: 'Tesla',
+      price: '99,000',
+      like: true,
+      img: require('../assets/images/tesla.jpg'),
+      about:
+        'The Model S of Tesla was the first mass-market car to prove that EVs could be exciting to drive, and useable for long trips.',
+    },
+  ];
+  
+  export default cars;
